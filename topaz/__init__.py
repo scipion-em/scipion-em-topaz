b383a6e0e40fc816ada27c55ebab80148135729a
Grigory Sharov <sharov.grigory@gmail.com>
2020-06-07 14:12:43 +0100

    gplv3

--- topaz/__init__.py
+++ topaz/__init__.py
@@ -8,7 +8,7 @@
 # *
 # * This program is free software; you can redistribute it and/or modify
 # * it under the terms of the GNU General Public License as published by
-# * the Free Software Foundation; either version 2 of the License, or
+# * the Free Software Foundation; either version 3 of the License, or
 # * (at your option) any later version.
 # *
 # * This program is distributed in the hope that it will be useful,
@@ -27,9 +27,11 @@
 # **************************************************************************
 
 import os
+
 import pwem
 import pyworkflow.utils as pwutils
 import pyworkflow as pw
+
 from .constants import *
 
 
